8ad33d3ce1a2d8872423a5f8f9750bad343ef576
Семён Каунов <kaunov.semyon@gmail.com>
2024-03-03 16:58:05 +0300

    update main.py from PoetFromIce

--- HW3/Kaunov/main.py
+++ HW3/Kaunov/main.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     text: str
+
 
 app = FastAPI()
 translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
 
 @app.post("/translate/")
 def translate(item: Item):
